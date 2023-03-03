4fef6676da39ebc1293744b9bef7c84ee9b4703b
kuingggg <63944062+kuingggg@users.noreply.github.com>
2023-03-04 05:15:47 +0800

    Update tex-svg.js

--- 5d6d/mjx3/tex-svg.js
+++ 5d6d/mjx3/tex-svg.js
@@ -1,3 +1,8 @@
 var jss = document.createElement("script");
 jss.src = "https://unpkg.com/mathjax@3.2.2/es5/tex-svg.js";
-document.head.appendChild(jss);
+document.getElementsByTagName("head")[0].appendChild(jss);
+
+var mee = document.createElement("meta");
+mee.name = "viewport";
+mee.content = "width=device-width, initial-scale=1;
+document.getElementsByTagName("head")[0].appendChild(mee);
